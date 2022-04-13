0abdbc66110a32246492860cc06d0fd689a2b10f
W. Guilherme <wguilhermecriativo@gmail.com>
2022-04-13 17:55:55 -0300

    feat: add @ decorator to components

--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -1,6 +1,6 @@
+import { Header } from '@/components'
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import { Header } from '../components'
 
 const Home: NextPage = () => {
   return (
